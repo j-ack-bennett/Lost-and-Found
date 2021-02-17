2c78e2ad7c975550a553af020b66defe554d8450
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-18 11:42:11 +1300

    Found route written

--- server/routes/found.js
+++ server/routes/found.js
@@ -6,18 +6,25 @@ const router = express.Router()
 
 router.get('/found', (req, res) => {
   db.getFound()
-      .then(animals => {
-          res.json({animals})
-      })
-      .catch(err => {
-        console.log(err)
-        res.status(500).json({ message: 'Something went wrong' })
-      })
+    .then(animals => {
+      res.json({ animals })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
 })
 
 router.post('/found', (req, res) => {
-  db.thatsMyPet()
-  
+  db.addFound(req.body.species, req.body.photo)
+    .then(() => {
+      res.status(200).json({ message: 'OK' })
+      return null
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
 })
 
 
