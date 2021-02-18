a9f01866dee2337037b3a8cc110e05045665462f
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-02-18 14:23:56 +1300

    Found API written

--- client/apis/found.js
+++ client/apis/found.js
@@ -1,3 +1,36 @@
 import request from 'superagent'
 
+import { getAuthorizationHeader } from 'authenticare/client'
+const acceptJsonHeader = { Accept: 'application/json' }
+
 const rootUrl = 'api/v1/found'
+
+export function getFound() {
+  return request
+    .get(rootURL)
+    .set(acceptJsonHeader)
+    .then(res => res.body.animals)
+    .catch(logError)
+}
+
+export function addFound(species, photo) {
+  return request 
+    .post(rootUrl)
+    .set(getAuthorizationHeader())
+    .send(species, photo)
+    .then(res => res.body.animals)
+    .catch(logError)
+}
+
+function logError (err) {
+  if (err.message === 'Forbidden') {
+    throw new Error('Only the user who added the fruit may update and delete it')
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(
+      'Error consuming the API (in client/apis/found.js):',
+      err.message
+    )
+    throw err
+  }
+}
