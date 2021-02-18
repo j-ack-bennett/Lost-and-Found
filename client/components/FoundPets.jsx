3f16d955148fc1cf50e8aee7b7048646ef1dfad9
student <student@eda-bc-03.localdomain>
2021-02-19 09:17:50 +1300

    pics on found work

---
+++ client/components/FoundPets.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { fetchFound } from '../actions/found'
+
+function FoundPets (props) {
+
+  useEffect(() => {
+    props.dispatch(fetchFound());
+  }, []);
+
+  console.log(props)
+  return (
+    <div>
+      {props.foundPets.map(animal => (
+        <div key={animal.id}>
+          <img className="foundPetImg" src={animal.photo} />
+          <div>
+            <h3>{animal.name}</h3>
+            {/* <h3>{animal.username}</h3> */}
+          </div>
+        </div> 
+      ))}        
+    </div>
+  )
+}
+
+   const mapStateToProps = (globalState) => {
+  return {
+    foundPets: globalState.foundPets
+  }
+};       
+
+
+export default connect(mapStateToProps)(FoundPets)
